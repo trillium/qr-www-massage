6f9b9b309525f67ec6d2af0f2925fa82f1c52c80
Trillium Smith <spiteless@gmail.com>
2024-08-10 23:17:07 -0700

    feat: Extend tailwind.config with screen, border, color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#2dd4bf",
+      },
+      screens: {
+        xxs: "360px", // Custom breakpoint for extra extra small screens
+        xss: "360px", // Custom breakpoint for extra extra small screens
+        xs: "480px", // Custom breakpoint for extra small screens
+      },
+      borderWidth: {
+        DEFAULT: "1px",
+        "0": "0",
+        "1": "1px",
+        "2": "2px",
+        "3": "3px",
+        "4": "4px",
+        "5": "5px",
+        "6": "6px",
+        "7": "7px",
+        "8": "8px",
+        "9": "9px",
+        "10": "10px",
+        "11": "11px",
+        "12": "12px",
+        "13": "13px",
       },
     },
   },
